b3e8af3a7e5ebf0f7a21cb98e6304353916b7ded
J0hn-B <connect-wsman@hotmail.com>
2020-10-26 14:56:07 +0200

    added cert-manager

--- scripts/aks.sh
+++ scripts/aks.sh
@@ -21,7 +21,6 @@ kubectl port-forward svc/argocd-server -n argocd 8080:443
 
 
 
-
 ******************************************************************
 # With stopped AKS cluster zero charges apply
 
