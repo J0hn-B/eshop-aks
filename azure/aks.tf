7cee879815aaa28bbd88ff0b45a7b4632474ab01
J0hn-B <connect-wsman@hotmail.com>
2020-11-12 20:37:19 +0000

    remove unused resources

--- azure/aks.tf
+++ azure/aks.tf
@@ -1,3 +1,16 @@
+# Create AKS Ingress Public IP
+resource "azurerm_public_ip" "ingress_ip_01" {
+  name                = "ingress-ip-01"
+  resource_group_name = azurerm_resource_group.aks.name
+  location            = azurerm_resource_group.aks.location
+  allocation_method   = "Static"
+  domain_name_label   = "dev-aks-eshop"
+  sku                 = "Standard"
+
+  tags = {
+    Environment = "Development"
+  }
+}
 
 # Create the vnet for the cluster
 resource "azurerm_virtual_network" "k8s" {
--- azure/storage.tf
+++ azure/storage.tf
@@ -1,19 +1,19 @@
-# Create the file share  for the cluster
-resource "azurerm_storage_account" "file_share" {
-  name                     = "cluster01shared01dev"
-  resource_group_name      = azurerm_resource_group.aks.name
-  location                 = azurerm_resource_group.aks.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+# # Create the file share  for the cluster
+# resource "azurerm_storage_account" "file_share" {
+#   name                     = "cluster01shared01dev"
+#   resource_group_name      = azurerm_resource_group.aks.name
+#   location                 = azurerm_resource_group.aks.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
 
-  tags = {
-    Environment = "Development"
-  }
-}
+#   tags = {
+#     Environment = "Development"
+#   }
+# }
 
-resource "azurerm_storage_share" "file_share" {
-  name                 = "k8share"
-  storage_account_name = azurerm_storage_account.file_share.name
-  quota                = 50
+# resource "azurerm_storage_share" "file_share" {
+#   name                 = "k8share"
+#   storage_account_name = azurerm_storage_account.file_share.name
+#   quota                = 50
 
-}
+# }
