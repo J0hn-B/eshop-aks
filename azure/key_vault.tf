660cc0af3676bd20d84a15169c2d6afff9e12f49
J0hn-B <connect-wsman@hotmail.com>
2020-11-13 14:44:56 +0000

    update id

--- azure/key_vault.tf
+++ azure/key_vault.tf
@@ -1,12 +1,21 @@
 # Create Azure Key Vault for the AKS cluster
-data "azurerm_client_config" "current" {}
+
+data "azurerm_key_vault_secret" "terr_sp_tenant_id" {
+  name         = "tenant"
+  key_vault_id = data.azurerm_key_vault.vault_01.id
+}
+
+data "azurerm_key_vault_secret" "terr_sp_object_id" {
+  name         = "object-id"
+  key_vault_id = data.azurerm_key_vault.vault_01.id
+}
 
 resource "azurerm_key_vault" "aks_kv_01" {
   name                        = "aks-rotation-key-vault"
   location                    = azurerm_resource_group.aks.location
   resource_group_name         = azurerm_resource_group.aks.name
   enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  tenant_id                   = data.azurerm_key_vault_secret.terr_sp_tenant_id.value
   soft_delete_enabled         = true
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
@@ -14,8 +23,8 @@ resource "azurerm_key_vault" "aks_kv_01" {
   sku_name = "standard"
 
   access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+    tenant_id = data.azurerm_key_vault_secret.terr_sp_tenant_id.value
+    object_id = data.azurerm_key_vault_secret.terr_sp_object_id.value
 
     key_permissions = [
       "get",
@@ -37,6 +46,8 @@ resource "azurerm_key_vault" "aks_kv_01" {
       "delete"
     ]
   }
+
+
 
   network_acls {
     default_action = "Allow"
