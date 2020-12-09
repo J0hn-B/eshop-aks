ade8ad80092dfba07ada7270e1f627eccf3dbb68
J0hn-B <connect-wsman@hotmail.com>
2020-12-09 14:24:56 +0000

    instructions update

--- scripts/argocd_install.sh
+++ scripts/argocd_install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 CYAN='\033[1;36m'
 GREEN='\033[0;32m'
 LBLUE='\033[1;34m'
 NC='\033[0m'
 # Create namespace, install ArgoCD and port-forward. Run the script from your laptop.
-kubectl create namespace argocd;
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml;
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 echo -e "${LBLUE}Waiting for ArgoCD installation ...... :)${NC}\n"
 kubectl wait --for=condition=Available --timeout=360s deploy/argocd-server -n argocd
@@ -17,4 +16,4 @@ PASS=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o nam
 
 echo -e "${CYAN}--> This is your username:${NC}${GREEN} admin${NC}\n"
 echo -e "${CYAN}--> This is your password:${NC}${GREEN} ${PASS}${NC}\n"
-echo -e "${LBLUE}--> Access argocd web ui in:${NC}${GREEN} localhost:8080${NC}\n"
+echo -e "${LBLUE}--> Access argocd web ui:${NC}${GREEN} kubectl port-forward svc/argocd-server -n argocd 8080:443 ${NC}--> in:${NC}${GREEN} localhost:8080${NC}\n"
